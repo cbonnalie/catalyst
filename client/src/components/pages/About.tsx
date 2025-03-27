c1cfadf782031fea826727c2410141c696544c93
cbonnalie <christianbonnalie@gmail.com>
2025-03-27 14:32:57 -0700

    Added client & server directories.

---
+++ client/src/components/pages/About.tsx
@@ -0,0 +1,15 @@
+﻿import Header from '../common/Header'
+
+function About() {
+    return (
+        <>
+            <Header />
+            <main className="container">
+                <h1>About</h1>
+                <p>Learn more about our application and our team.</p>
+            </main>
+        </>
+    );
+}
+
+export default About;
