3441c43ac8a4a6eb172417e5c14046e1dbaf424e
cbonnalie <christianbonnalie@gmail.com>
2025-03-27 15:25:34 -0700

    Server configuration.

---
+++ server/server.ts
@@ -0,0 +1,17 @@
+﻿import express from "express"
+import cors from "cors"
+
+const app = express()
+// allows frontend requests
+app.use(cors())
+// allows req.body
+app.use(express.json())
+
+const PORT = 3000
+
+app.get("/api", async (req, res) => {
+    console.log("/api route hit")
+    res.json({message: "API is working!"})
+})
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`))
