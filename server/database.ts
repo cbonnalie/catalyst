2fe01c5cb6bbecd09aa3a272f7035adf77a86eba
cbonnalie <christianbonnalie@gmail.com>
2025-03-27 17:07:57 -0700

    DB connection init.

---
+++ server/database.ts
@@ -0,0 +1,29 @@
+﻿import sqlite3 from 'sqlite3'
+import {open, Database} from 'sqlite'
+import path from 'path'
+
+// absolute path to db
+const dbPath = path.resolve(__dirname, '..', 'databases', 'events.db')
+
+// define a type for the db connection
+let db: Database | null = null
+
+// init db connection
+export async function initializeDatabase(): Promise<void> {
+    try {
+        db = await open({
+            filename: dbPath,
+            driver: sqlite3.Database,
+        })
+    } catch (err) {
+        console.error("Error initializing database:", err)
+        throw err
+    }
+}
+
+export function getDatabase(): Database {
+    if (!db) {
+        throw new Error('Database does not exist. Call initializeDatabase() first')
+    }
+    return db
+}
--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,6 @@
 ﻿import express from "express"
 import cors from "cors"
+import {initializeDatabase, getDatabase} from "./database"
 
 const app = express()
 // allows frontend requests
@@ -7,11 +8,32 @@ app.use(cors())
 // allows req.body
 app.use(express.json())
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
-app.get("/api", async (req, res) => {
+async function startServer() {
+    try {
+        await initializeDatabase()
+    } catch (err) {
+        console.log("Error fetching database", err)
+    }
+}
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`))
+
+// test API calls
+app.get("/api", async (_, res) => {
     console.log("/api route hit")
     res.json({message: "API is working!"})
 })
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`))
+app.get("/db", async (req, res) => {
+    try {
+        const db = getDatabase()
+        console.log(db)
+        console.log("getDB succeeded")
+    } catch (err) {
+        console.log("Error fetching database", err)
+    }
+})
+
+startServer()
